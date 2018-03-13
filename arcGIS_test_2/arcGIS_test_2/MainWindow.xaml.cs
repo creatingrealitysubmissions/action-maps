c8257270d60a68d5b11ad75c108f823e21906c89
Jeff Katz <kraln@kraln.com>
2018-03-13 15:21:17 -0700

    stress test

--- arcGIS_test_2/arcGIS_test_2/MainWindow.xaml.cs
+++ arcGIS_test_2/arcGIS_test_2/MainWindow.xaml.cs
@@ -27,8 +27,9 @@ namespace arcGIS_test_2
     /// </summary>
     public partial class MainWindow : Window
     {
-        // la is 34.02209° N, -118.2852° E
-        Graphic buoyTest;
+        // USC is 34.02209° N, -118.2852° E
+        // intercontinental is 34.0498° N, -118.2606° E
+        // SD is -117.161087, 32.715736
 
         public MainWindow()
         {
@@ -38,8 +39,9 @@ namespace arcGIS_test_2
             MySceneView.Scene = new Scene(Basemap.CreateTopographic());
              
             // Add San Diego scene layer.  Example scene layers provided by Esri available here: http://www.arcgis.com/home/group.html?id=c4a19ab700fd4654b89a319b016eee03
-            //MySceneView.Scene.OperationalLayers.Add(new ArcGISSceneLayer(new System.Uri("https://tiles.arcgis.com/tiles/Imiq6naek6ZWdour/arcgis/rest/services/San_Diego_Textured_Buildings/SceneServer/layers/0")));
-            MySceneView.Scene.OperationalLayers.Add(new ArcGISSceneLayer(new System.Uri("http://scene.arcgis.com/arcgis/rest/services/Hosted/Buildings_Brest/SceneServer/layers/0")));
+            
+            MySceneView.Scene.OperationalLayers.Add(new ArcGISSceneLayer(new System.Uri("https://tiles.arcgis.com/tiles/Imiq6naek6ZWdour/arcgis/rest/services/San_Diego_Textured_Buildings/SceneServer/layers/0")));
+            //MySceneView.Scene.OperationalLayers.Add(new ArcGISSceneLayer(new System.Uri("http://scene.arcgis.com/arcgis/rest/services/Hosted/Buildings_Brest/SceneServer/layers/0")));
 
             // Add elevation surface from ArcGIS Online
             MySceneView.Scene.BaseSurface.ElevationSources.Add(new ArcGISTiledElevationSource(new System.Uri("https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")));
@@ -53,49 +55,35 @@ namespace arcGIS_test_2
             MySceneView.SetViewpointCamera(camera);
 
             var wgs84 = MySceneView.Scene.SpatialReference;
-
-            // USC is 34.02209° N, -118.2852° E
-            // intercontinental is 34.0498° N, -118.2606° E
-            var buoy1Loc = new MapPoint(-118.2606, 34.0498, 1000, wgs84);
-            
-
-            // create a marker symbol
-            var buoyMarker = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, Colors.Red, 10);
-
-            // create graphics
-            var buoyTest = new Graphic(buoy1Loc, buoyMarker);
-
             GraphicsOverlay go = new GraphicsOverlay();
             go.SceneProperties.SurfacePlacement = SurfacePlacement.Relative;
-            go.Graphics.Add(buoyTest);
-            MySceneView.GraphicsOverlays.Add(go);
+            var rnd = new System.Random();
 
-            /* Make the intercontinental marked */
-            Task.Factory.StartNew(async () => {
-                while (true)
-                {
-                    buoyTest.Geometry = new MapPoint(-118.2606, 34.0498, (new System.Random()).Next(0, 1000));
-                    await Task.Delay(100);
-                }
-            });
-
-            /* Load Bus stops */
-            Task.Factory.StartNew(async () =>
+            for (int i = 0; i < 10000; i++)
             {
 
 
-                // bus stops
-                Uri u = new System.Uri("https://services1.arcgis.com/gOY38HDnFUYPDony/arcgis/rest/services/la_busstops1217/FeatureServer");
-                ServiceFeatureTable sft = new ServiceFeatureTable(u);
+                var buoy1Loc = new MapPoint(-118.2606, 34.0498, 1000, wgs84);
+            
+                // create a marker symbol
+                var buoyMarker = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, Colors.Red, 10);
 
+                // create graphics
+                var buoyTest = new Graphic(buoy1Loc, buoyMarker);
 
-                await sft.LoadAsync();
+                go.Graphics.Add(buoyTest);
 
-                MySceneView.Scene.OperationalLayers.Add(new FeatureLayer(sft));
+                /* Make the intercontinental marked */
+                Task.Factory.StartNew(async () => {
+                    while (true)
+                    {
+                        buoyTest.Geometry = new MapPoint(-118.2606 + rnd.NextDouble(), 34.0498 + rnd.NextDouble(), rnd.Next(0, 1000));                    
+                        await Task.Delay(100);
+                    }
+                });
 
-            });
+            }
+            MySceneView.GraphicsOverlays.Add(go);
         }
-
-        
     }
 }
